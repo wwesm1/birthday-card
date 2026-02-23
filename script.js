88cf9d1de63ea7bbba854c73516b21f8afff5c43
wwesm1 <hulkaroynormurodova1234@gmail.com>
2026-02-23 22:40:56 +0500

    some changes

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-const btn = document.getElementById("wishBtn");
-const msg = document.getElementById("message");
+const btn = document.getElementById("open");
+const letter = document.getElementById("letter");
 
-btn.addEventListener("click", () => {
-  msg.classList.remove("hidden");
-  btn.textContent = "I love you bestie 💗";
-});
+btn.onclick = ()=>{
+ letter.classList.remove("hidden");
+ btn.style.display="none";
+};
+
+/* generate luxury stars */
+
+const starField=document.querySelector(".stars");
+
+for(let i=0;i<120;i++){
+ const s=document.createElement("span");
+ s.style.top=Math.random()*100+"vh";
+ s.style.left=Math.random()*100+"vw";
+ s.style.animationDelay=Math.random()*3+"s";
+ starField.appendChild(s);
+}
